bd7880b2fcfbdf968fdee52ad9c12f396c0bef4a
Ankit Singh <ankitsingh91@gmail.com>
2023-04-12 23:59:35 +0530

    Initialized

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './pages/header/header.component';
+import { RecipesComponent } from './pages/recipes/recipes.component';
+import { RecipeListComponent } from './pages/recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './pages/recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './pages/recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './pages/shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './pages/shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
